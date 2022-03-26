1ba7ed4b11c28a0210698451b1eb68445ad7c3a4
C Scott <cscottdev@protonmail.com>
2022-03-26 17:12:45 -0500

    added smarter logic for initializing tor

--- app/scripts/start.sh
+++ app/scripts/start.sh
@@ -1,5 +1,19 @@
 ## Start script
+LOGFILE="/tmp/tor.log"
+
+# Install dependencies if not present
 yarn install --check-files
-tor & sleep 10
-echo "Onion address: $(cat /root/data/tor/hostname)"
-node --es-module-specifier-resolution=node --no-warnings app.js
+
+# Initialize Tor
+echo "Starting Tor server..."
+pkill tor
+tor > $LOGFILE & TORPID=$!
+
+# Wait for Tor to load, then start node.
+tail -fn0 $LOGFILE | while read line; do
+  echo "$line" | grep "Bootstrapped 100%"
+  if [ $? = 0 ]; then
+    echo "onion address: $(cat /root/data/tor/hostname)"
+    node --es-module-specifier-resolution=node --no-warnings app.js
+  fi
+done
